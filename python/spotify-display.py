f749c91feca887fe9c0b2a46b2db52060fdc4147
Michael Cramer <cramer@webkist.com>
2021-06-10 14:09:41 -0400

    scrolling the spotify data!

--- python/spotify-display.py
+++ python/spotify-display.py
@@ -11,47 +11,61 @@ options = RGBMatrixOptions()
 options.hardware_mapping = "adafruit-hat"
 options.rows = 32
 options.cols = 64
-options.chain_length = 1
-options.parallel = 1
-options.row_address_type = 0
-options.multiplexing = 0
-options.pwm_bits = 11
-options.brightness = 100
-options.pwm_lsb_nanoseconds = 130
-options.led_rgb_sequence = "RGB"
-options.pixel_mapper_config = ""
-options.panel_type = ""
 options.disable_hardware_pulsing = True
 
 matrix = RGBMatrix(options=options)
 
 offscreen_canvas = matrix.CreateFrameCanvas()
 font = graphics.Font()
-font.LoadFont("Commodore_ruby_12.bdf")
-textColor = graphics.Color(255, 255, 255)
+font.LoadFont("font.bdf")
+textColor = graphics.Color(0, 128, 0)
 pos = offscreen_canvas.width
 
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.environ["SPOTIFY_ID"],
+                                               client_secret=os.environ["SPOTIFY_SECRET"],
+                                               redirect_uri="http://localhost:8080/callback",
+                                               scope="user-library-read,user-read-playback-state"))
+
+user = sp.current_user()
+print("Now Playing for %s [%s]\n" % (user["display_name"], user["id"]))
+
 while True:
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.environ["SPOTIFY_ID"],
-                                                   client_secret=os.environ["SPOTIFY_SECRET"],
-                                                   redirect_uri="http://localhost:8080/callback",
-                                                   scope="user-library-read,user-read-playback-state"))
-
-    user = sp.current_user()
-    print("Now Playing for %s [%s]\n" % (user["display_name"], user["id"]))
-
     np = sp.current_user_playing_track()
 
     if np["is_playing"]:
-        print("\n%s\n%s\n%s\n" % (np["item"]["album"]["name"], np["item"]["name"], np["item"]["artists"][0]["name"] ))
+        line1 = np["item"]["name"]
+        line2 = np["item"]["album"]["name"]
+        line3 = np["item"]["artists"][0]["name"]
+    else:
+        line1 = "Nothing"
+        line2 = "playing..."
+        line3 = ""
 
-    while pos > 0:
+    length = max(graphics.DrawText(offscreen_canvas, font, pos, 9, textColor, line1),
+                 graphics.DrawText(offscreen_canvas, font, pos, 19, textColor, line2),
+                 graphics.DrawText(offscreen_canvas, font, pos, 29, textColor, line3))
+
+    if length > pos:
+        print("%d vs %d" % (length, pos))
+        while pos + length > 0:
+            offscreen_canvas.Clear()
+
+            graphics.DrawText(offscreen_canvas, font, pos, 9, textColor, line1)
+            graphics.DrawText(offscreen_canvas, font, pos, 19, textColor, line2)
+            graphics.DrawText(offscreen_canvas, font, pos, 29, textColor, line3)
+
+            pos -= 1
+            time.sleep(0.07)
+            offscreen_canvas = matrix.SwapOnVSync(offscreen_canvas)
+    else:
+        print("%d vs %d" % (length, pos))
         offscreen_canvas.Clear()
-        len = graphics.DrawText(offscreen_canvas, font, pos, 10, textColor, np["item"]["name"])
-        pos -= 1
-        time.sleep(0.05)
+
+        graphics.DrawText(offscreen_canvas, font, 2, 9, textColor, line1)
+        graphics.DrawText(offscreen_canvas, font, 2, 19, textColor, line2)
+        graphics.DrawText(offscreen_canvas, font, 2, 29, textColor, line3)
+
         offscreen_canvas = matrix.SwapOnVSync(offscreen_canvas)
+        time.sleep(5)
 
     pos = offscreen_canvas.width
-
-# print(json.dumps(np))
