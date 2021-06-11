b86cda68eab4a56b074a0086c29a40bdf764a4c2
Michael Cramer <cramer@webkist.com>
2021-06-11 14:35:29 -0400

    progress

--- python/spotify-display.py
+++ python/spotify-display.py
@@ -1,9 +1,24 @@
 import spotipy
 import time
 import os
+import os.path
 from spotipy.oauth2 import SpotifyOAuth
 import simplejson
 from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
+from PIL import Image, ImageOps
+import urllib
+
+def getImage(url):
+    m = url.rsplit('/', 1)
+    print(m[-1])
+    filename = "imagecache/%s" % m[-1]
+    if not os.path.isfile(filename):
+        print("Getting %s" % url)
+        urllib.urlretrieve(url, filename)
+
+    image = Image.open(filename)
+    image.thumbnail((28, 28), Image.NEAREST)
+    return image
 
 # --led-no-hardware-pulse=1 --led-cols=64 --led-rows=32 --led-gpio-mapping=adafruit-hat
 
@@ -18,7 +33,7 @@ matrix = RGBMatrix(options=options)
 offscreen_canvas = matrix.CreateFrameCanvas()
 font = graphics.Font()
 font.LoadFont("font.bdf")
-textColor = graphics.Color(64, 0, 0)
+textColor = graphics.Color(128, 128, 128)
 red = 0
 green = 0
 blue = 0
@@ -42,28 +57,34 @@ while True:
         time.sleep(5)
         continue
 
-
     if np and np["is_playing"]:
         line1 = np["item"]["name"]
         line2 = np["item"]["album"]["name"]
         line3 = np["item"]["artists"][0]["name"]
         print("%s" % np["item"]["album"]["images"][2]["url"])
+        image = getImage(np["item"]["album"]["images"][2]["url"])
+
     else:
         line1 = "Nothing"
         line2 = ""
         line3 = "playing..."
+        image = getImage("https://freeiconshop.com/wp-content/uploads/edd/cloud-outline.png")
+        image = ImageOps.invert(image.convert('RGB'))
 
-    length = max(graphics.DrawText(offscreen_canvas, font, pos, 15, textColor, line1),
-                 graphics.DrawText(offscreen_canvas, font, pos, 25, textColor, line3))
+    length = max(graphics.DrawText(offscreen_canvas, font, 0, 15, textColor, line1),
+                 graphics.DrawText(offscreen_canvas, font, 0, 25, textColor, line3))
+
+    length += 32
 
     if length > pos:
         # If the text is wider than the view, scroll it.
         while pos + length > 0:
             offscreen_canvas.Clear()
             offscreen_canvas.Fill(red, green, blue)
+            offscreen_canvas.SetImage(image.convert('RGB'), 34, 2)
 
-            graphics.DrawText(offscreen_canvas, font, pos, 15, textColor, line1)
-            graphics.DrawText(offscreen_canvas, font, pos, 25, textColor, line3)
+            graphics.DrawText(offscreen_canvas, font, pos + 32, 15, textColor, line1)
+            graphics.DrawText(offscreen_canvas, font, pos + 32, 25, textColor, line3)
 
             pos -= 1
             time.sleep(0.07)
